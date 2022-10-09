c867b6fe9fa667f0c746a351790bd9dc0bebf2ac
AmError <74107109+AmmError@users.noreply.github.com>
2022-10-09 10:09:58 +0530

    Update main.py

---
+++ PassGen/main.py
@@ -0,0 +1,31 @@
+# Imports
+import secrets
+import string
+
+# Defining Alphabets
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+
+alphabet = letters + digits + special_chars
+
+# Pass Length (By Default Settled 12)
+pwd_length = 12 # Change This to increase/decrese the length of the password
+
+# Generates a Password String
+pwd = ''
+for i in range(pwd_length):
+  pwd += ''.join(secrets.choice(alphabet))
+
+print(pwd)
+
+# Generates Password Meeting Constriants
+while True:
+  pwd = ''
+  for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(alphabet))
+
+  if (any(char in special_chars for char in pwd) and 
+      sum(char in digits for char in pwd)>=2):
+          break
+print(pwd)
